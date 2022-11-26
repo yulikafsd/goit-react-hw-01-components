7eee1655cd940224c4cbcb4b00b4f7de7e31ac5f
Yulika Zagorovsky <ju.zagorovsky@gmail.com>
2022-11-26 22:11:23 +0100

    bugfix className

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,26 +1,26 @@
 export default function Profile({ username, tag, location, avatar, stats }) {
   const { followers, views, likes } = stats;
   return (
-    <div class="profile">
-      <div class="description">
-        <img src={avatar} alt="User avatar" class="avatar" />
-        <p class="name">{username}</p>
-        <p class="tag">@{tag}</p>
-        <p class="location">{location}</p>
+    <div className="profile">
+      <div className="description">
+        <img src={avatar} alt="User avatar" className="avatar" />
+        <p className="name">{username}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
       </div>
 
-      <ul class="stats">
+      <ul className="stats">
         <li>
-          <span class="label">Followers</span>
-          <span class="quantity">{followers}</span>
+          <span className="label">Followers</span>
+          <span className="quantity">{followers}</span>
         </li>
         <li>
-          <span class="label">Views</span>
-          <span class="quantity">{views}</span>
+          <span className="label">Views</span>
+          <span className="quantity">{views}</span>
         </li>
         <li>
-          <span class="label">Likes</span>
-          <span class="quantity">{likes}</span>
+          <span className="label">Likes</span>
+          <span className="quantity">{likes}</span>
         </li>
       </ul>
     </div>
--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,16 +1,13 @@
-// Колір фону елемента статистики в оформленні можна пропустити або створити функцію для генерації випадкового кольору.
-
 export default function Statistics({ title, stats }) {
-  //   const { id, label, percentage } = stats;
   return (
-    <section class="statistics">
-      {title && <h2 class="title">{title}</h2>}
-      <ul class="stat-list">
+    <section className="statistics">
+      {title && <h2 className="title">{title}</h2>}
+      <ul className="stat-list">
         {stats.map(({ id, label, percentage }) => {
           return (
-            <li class="item" key={id}>
-              <span class="label">{label}</span>
-              <span class="percentage">{percentage}</span>
+            <li className="item" key={id}>
+              <span className="label">{label}</span>
+              <span className="percentage">{percentage}</span>
             </li>
           );
         })}
