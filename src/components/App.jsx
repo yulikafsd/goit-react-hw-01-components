e49482cfdfc5affb7987a5fe26a077f56cdddd0f
Yulika Zagorovsky <ju.zagorovsky@gmail.com>
2022-11-26 19:52:09 +0100

    add profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import user from './profile/user.json';
+import data from './statistics/data.json';
+
+import Profile from './profile/Profile';
+import Statistics from './statistics/Statistics';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+    </>
   );
 };
---
+++ src/components/profile/Profile.js
@@ -0,0 +1,28 @@
+export default function Profile({ username, tag, location, avatar, stats }) {
+  const { followers, views, likes } = stats;
+  return (
+    <div class="profile">
+      <div class="description">
+        <img src={avatar} alt="User avatar" class="avatar" />
+        <p class="name">{username}</p>
+        <p class="tag">@{tag}</p>
+        <p class="location">{location}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
