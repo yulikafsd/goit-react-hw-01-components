efeec44f9f9b56f9682f02df70f35a46fb645fa9
Yulika Zagorovsky <ju.zagorovsky@gmail.com>
2022-11-28 00:05:03 +0100

    bugfix prop-types friends

--- src/components/friends/FriendList.js
+++ src/components/friends/FriendList.js
@@ -20,5 +20,12 @@ export default function FriendList({ friends }) {
 }
 
 FriendList.propTypes = {
-  friends: PropTypes.arrayOf(PropTypes.object).isRequired,
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
 };
